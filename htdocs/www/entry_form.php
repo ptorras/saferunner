95d547a69681ed915ce323cb8ea5cc5469dcd1bc
Adrià Carrasquilla <carrasquilla.adria@gmail.com>
2020-05-21 18:46:47 +0200

    add jQuery

--- htdocs/www/entry_form.php
+++ htdocs/www/entry_form.php
@@ -92,6 +92,6 @@
                     require_once(__DIR__.'/secret/key.php');
                     echo $key;
                 ?>&callback=initMap"> </script>
-
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     </body>
 </html>
