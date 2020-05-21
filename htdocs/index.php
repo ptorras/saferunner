7bba3118756a6728e85a6978f18745db10d8fac8
Pau Torras <57144976+RostarFreeman@users.noreply.github.com>
2020-05-21 18:15:55 +0200

    Adapted to google app engine directory structure (for realzies this time) BUT MAPS DOESNT WORK

--- htdocs/index.php
+++ htdocs/index.php
@@ -2,12 +2,12 @@
 // Entry point per a que l'App engine detecti els path
 switch (@parse_url($_SERVER['REQUEST_URI'])['path']) {
     case '/':
-        require_once __DIR__.'/entry_form.php';
+        require_once __DIR__.'/www/entry_form.php';
         break;
     case '/makeroute':
         break;
     case '/test':
-        require_once __DIR__.'/test.php';
+        require_once __DIR__.'/www/test.php';
         break;
     default:
         http_response_code(404);
