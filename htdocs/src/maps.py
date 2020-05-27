0e3fc7bed5625a0545023023e5d9fe3df87d64e9
Adrià Carrasquilla <carrasquilla.adria@gmail.com>
2020-05-27 17:32:54 +0200

    Path script init working

--- htdocs/src/maps.py
+++ htdocs/src/maps.py
@@ -5,9 +5,12 @@ from ipywidgets.embed import embed_minimal_html
 import json
 from secret import key
 import cgi
+import math
+import random
 
 form = cgi.FieldStorage()
-print('form')
+
+R = 6373.0  # Earth radius
 
 gmap = googlemaps.Client(key=key)
 gmaps.configure(api_key=key)
@@ -15,46 +18,116 @@ gmaps.configure(api_key=key)
 # TODO: get this from client
 now = datetime.now()
 origin = (41.620748, 2.295780)
-destination = origin  # (41.622039, 2.296327)
-distance = ""
+origin = (41.564166, 1.999159)
+
+distance = 5000
+places_radius = distance/(math.pi)
 date = ""
 time = ""
 pace = ""
 city = ""
 
-# TODO: choose safest 3 waypoints
-waypoints = [(41.622024, 2.297400), (41.620972, 2.296649), (41.621285, 2.296906)]
+# Get interesting points
+parks = gmap.places_nearby(origin, places_radius, type='park')
+stadiums = gmap.places_nearby(origin, places_radius, open_now=False, type='stadium')
+attraction = gmap.places_nearby(origin, places_radius, open_now=False, type='tourist_attraction')
+natural = gmap.places_nearby(origin, places_radius, open_now=False, type='natural_feature')
 
-directions_result = gmap.directions(origin, destination, waypoints=waypoints, mode='walking', departure_time=now)[0]
+possible_waypoints = []
+
+for parc in parks['results']:
+    info = {'lat': parc['geometry']['location']['lat'], 'lng': parc['geometry']['location']['lng'],
+            'id': parc['id'], 'name': parc['name']}
+    possible_waypoints.append(info)
+
+for stadium in stadiums['results']:
+    info = {'lat': stadium['geometry']['location']['lat'], 'lng': stadium['geometry']['location']['lng'],
+            'id': stadium['id'], 'name': stadium['name']}
+    possible_waypoints.append(info)
+
+for attr in attraction['results']:
+    info = {'lat': attr['geometry']['location']['lat'], 'lng': attr['geometry']['location']['lng'],
+            'id': attr['id'], 'name': attr['name']}
+    possible_waypoints.append(info)
+
+for nat in natural['results']:
+    info = {'lat': nat['geometry']['location']['lat'], 'lng': nat['geometry']['location']['lng'],
+            'id': nat['id'], 'name': nat['name']}
+    possible_waypoints.append(info)
 
 
+# If not enough places, generate random points
+while len(possible_waypoints) < 3:
+    # create random points
+    r = places_radius/(4*111300)        # radius conversion to degrees (also by 4 to reduce random area)
+    u = random.random()
+    v = random.random()
+    w = r * math.sqrt(u)
+    t = 2 * math.pi * v
+    x = w * math.cos(t)
+    y = w * math.sin(t)
+    x = x / math.cos(origin[1])
+    lat = x + origin[0]  # TODO
+    lng = y + origin[1]
+    info = {'lat': lat, 'lng': lng, 'id': 'RAND', 'name': 'RAND_POINT'}
+    possible_waypoints.append(info)
+    print((lat, lng))
+    pass
+
+for point in possible_waypoints:
+    print(point['name'])
+
+# TODO: Sort points by coincidences at same time
+sorted_waypoints = possible_waypoints
+
+# Select waypoints for the route
+waypoints = []
+total_distance = 0
+i = 0
+while (len(waypoints) < 3 or total_distance >= distance*1.3) and i < len(sorted_waypoints):
+    point = sorted_waypoints[i]
+    if i == 0:
+        total_distance += gmap.distance_matrix(origin, (point['lat'], point['lng']),
+                                               mode='walking')['rows'][0]['elements'][0]['distance']['value']
+        waypoints.append((point['lat'], point['lng']))
+    else:
+        if len(waypoints) == 2:
+            org = (point['lat'], point['lng'])
+            dest = origin
+        else:
+            org = waypoints[-1]
+            dest = (point['lat'], point['lng'])
+
+        dist = gmap.distance_matrix(org, dest, mode='walking')['rows'][0]['elements'][0]['distance']['value']
+        if total_distance + dist <= total_distance*1.3:    # variation possibility
+            total_distance += dist
+            waypoints.append((point['lat'], point['lng']))
+    i += 1
+
+# Fill with random if not full and distance not covered
+while len(waypoints) < 3 and distance*0.7 >= total_distance >= distance*1.3:
+    rand_choice = random.randint(0, len(sorted_waypoints))
+    waypoints.append(sorted_waypoints[rand_choice])
+
+print(waypoints)
+
+
+directions_result = gmap.directions(origin, origin, waypoints=waypoints, mode='walking', departure_time=now)[0]
 print(json.dumps(directions_result, indent=4))
 print("Number of legs {}".format(len(directions_result)))
-
-# fig.add_layer(directions_result)
-# embed_minimal_html('export.html', views=[fig])
 
 # Data structure to save path points coords
 path_coords = []
 for step in directions_result['legs']:
-    path_coords.append([step['start_location']['lat'], step['start_location']['lng']])
-    path_coords.append([step['end_location']['lat'], step['end_location']['lng']])
+    path_coords.append([step['start_location']['lat'], step['start_location']['lng'], step['distance']])
+    path_coords.append([step['end_location']['lat'], step['end_location']['lng'], step['distance']])
 
 # Visual check
 for n, point in enumerate(path_coords):
     print("Step {}".format(n))
     print("Latitude: {} \t Longitude: {}".format(point[0], point[1]))
+    print(point[2])
 
 
 # TODO: add db entry
 
-
-"""
-for step in directions_result["legs"]:
-    print(step['start_location'])
-for step in directions_result["legs"]:
-    print(step['end_location'])
-for step in directions_result["legs"]:
-    for s in (step['steps']):
-        print(s["html_instructions"])
-"""
