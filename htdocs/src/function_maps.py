4c3a47a4dd9634cbf5e058a4d488f9d581382ea6
Adrià Carrasquilla <carrasquilla.adria@gmail.com>
2020-05-28 20:27:04 +0200

    final py script

--- htdocs/src/function_maps.py
+++ htdocs/src/function_maps.py
@@ -53,7 +53,7 @@ def get_route(request):
     now = datetime.now()
     origin = (lat, lng)
 
-    distance = 5000
+    distance = 10000
     places_radius = distance / (math.pi)
     # date = datetime.now().strftime("%Y-%m-%d")
     # hour1 = datetime.now().strftime("%H:%M:%S")
@@ -73,28 +73,40 @@ def get_route(request):
 
     for parc in parks['results']:
         info = {'lat': parc['geometry']['location']['lat'], 'lng': parc['geometry']['location']['lng'],
-                'id': parc['id'], 'name': parc['name']}
+                'id': parc['id'], 'name': parc['name'],
+                'dist': gmap.distance_matrix(origin, (parc['geometry']['location']['lat'],
+                                                      parc['geometry']['location']['lng']),
+                                             mode='walking')['rows'][0]['elements'][0]['distance']['value']}
         possible_waypoints.append(info)
 
     for stadium in stadiums['results']:
         info = {'lat': stadium['geometry']['location']['lat'], 'lng': stadium['geometry']['location']['lng'],
-                'id': stadium['id'], 'name': stadium['name']}
+                'id': stadium['id'], 'name': stadium['name'],
+                'dist': gmap.distance_matrix(origin, (stadium['geometry']['location']['lat'],
+                                                      stadium['geometry']['location']['lng']),
+                                             mode='walking')['rows'][0]['elements'][0]['distance']['value']}
         possible_waypoints.append(info)
 
     for attr in attraction['results']:
         info = {'lat': attr['geometry']['location']['lat'], 'lng': attr['geometry']['location']['lng'],
-                'id': attr['id'], 'name': attr['name']}
+                'id': attr['id'], 'name': attr['name'],
+                'dist': gmap.distance_matrix(origin, (attr['geometry']['location']['lat'],
+                                                      attr['geometry']['location']['lng']),
+                                             mode='walking')['rows'][0]['elements'][0]['distance']['value']}
         possible_waypoints.append(info)
 
     for nat in natural['results']:
         info = {'lat': nat['geometry']['location']['lat'], 'lng': nat['geometry']['location']['lng'],
-                'id': nat['id'], 'name': nat['name']}
+                'id': nat['id'], 'name': nat['name'],
+                'dist': gmap.distance_matrix(origin, (nat['geometry']['location']['lat'],
+                                                      nat['geometry']['location']['lng']),
+                                             mode='walking')['rows'][0]['elements'][0]['distance']['value']}
         possible_waypoints.append(info)
 
     # If not enough places, generate random points
     while len(possible_waypoints) < 3:
         # create random points
-        r = places_radius / (4 * 111300)  # radius conversion to degrees (also by 4 to reduce random area)
+        r = places_radius / (4*111300)  # radius conversion to degrees (also by 4 to reduce random area)
         u = random.random()
         v = random.random()
         w = r * math.sqrt(u)
@@ -125,12 +137,13 @@ def get_route(request):
             punts = conn.execute(stmt, id=wp['id'], data=date, h1=hour1, h2=hour2).fetchall()
             wp['count'] = punts[0][0]
         conn.close()
-
-    sorted_waypoints = sorted(possible_waypoints, key=lambda d: d['count'])
+    sorted_waypoints = sorted(possible_waypoints, key=lambda d: d['dist'], reverse=True)
+    sorted_waypoints = sorted(sorted_waypoints, key=lambda d: d['count'])
     # return (str(sorted_waypoints), 200, headers)
 
     # Select waypoints for the route
     waypoints = []
+    waypoints_aux = []
     total_distance = 0
     i = 0
     while (len(waypoints) < 3 or total_distance >= distance * 1.3) and i < len(sorted_waypoints):
@@ -139,6 +152,7 @@ def get_route(request):
             total_distance += gmap.distance_matrix(origin, (point['lat'], point['lng']),
                                                    mode='walking')['rows'][0]['elements'][0]['distance']['value']
             waypoints.append((point['lat'], point['lng']))
+            waypoints_aux.append(point)
         else:
             if len(waypoints) == 2:
                 org = (point['lat'], point['lng'])
@@ -151,14 +165,15 @@ def get_route(request):
             if total_distance + dist <= total_distance * 1.3:  # variation possibility
                 total_distance += dist
                 waypoints.append((point['lat'], point['lng']))
+                waypoints_aux.append(point)
         i += 1
 
     # Fill with random if not full and distance not covered
     while len(waypoints) < 3 and distance * 0.7 >= total_distance >= distance * 1.3:
         rand_choice = random.randint(0, len(sorted_waypoints))
         waypoints.append(sorted_waypoints[rand_choice])
+        waypoints_aux.append(sorted_waypoints[rand_choice])
 
-    print(waypoints)
 
     directions_result = gmap.directions(origin, origin, waypoints=waypoints, mode='walking', departure_time=now)[0]
     print(json.dumps(directions_result, indent=4))
@@ -167,7 +182,9 @@ def get_route(request):
     # Data structure to save path points coords
     path_coords = []
     path_coords.append(directions_result['legs'][0]['start_location'].copy())
+    path_waypoints = []
     for step in directions_result['legs']:
+        path_waypoints.append(step['end_location'].copy())
         for step2 in step['steps']:
             path_coords.append(step2['end_location'].copy())
 
@@ -175,5 +192,30 @@ def get_route(request):
     for n, point in enumerate(path_coords):
         print("Step {}".format(n))
         print("Latitude: {} \t Longitude: {}".format(point['lat'], point['lng']))
+
+    # QUERY
+    # data = "YYYY-MM-DD"
+    # time_begin = "HH:MM:SS"
+    # info waypoint = waypoints ({'lat': lat, 'lng': lng, 'id': 'RAND', 'name': 'RAND_POINT', 'count': 0})
+
+    q_date = date + " " + time_begin
+
+    with db.connect() as conn:
+        stmt = sqlalchemy.text("INSERT INTO ruta (municipi, data, ritme) VALUES (:Municipi, :Data, :Ritme);")
+
+        conn.execute(stmt, Municipi="Municipi", Data=q_date, Ritme=2)
+
+        stmt = sqlalchemy.text("SELECT LAST_INSERT_ID() AS id;")
+
+        idRuta = conn.execute(stmt).fetchall()[0][0]
+
+        i = 0
+        for punt in waypoints_aux:
+            stmt = sqlalchemy.text(
+                "INSERT INTO punt (id_ruta,id_waypoint,nom_waypoint,data,latitud, longitud, ordre_a_ruta) VALUES (:id_ruta,:id_waypoint,:nom_waypoint,:Data2,:latitud, :longitud, :ordre_a_ruta);")
+            conn.execute(stmt, id_ruta=idRuta, id_waypoint=punt['id'], nom_waypoint=punt['name'], Data2=q_date,
+                         latitud=punt['lat'], longitud=punt['lng'], ordre_a_ruta=i)
+            i += 1
+        conn.close()
 
     return json.dumps(path_coords), 200, headers
