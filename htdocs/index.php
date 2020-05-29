02ecbbef020853cd971e2a76d8e597b7cf14b98c
janMoros <48655384+janMoros@users.noreply.github.com>
2020-05-29 11:27:22 +0200

    delete test option and comment clean up

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,14 +1,13 @@
 <?php
-require_once __DIR__.'/www/secret/key.php';
-// Entry point per a que l'App engine detecti els path
+// key.php is a secret file containing a variable declaration, for the MAPS API key. You have to manually change it to yours.
+require_once __DIR__.'/www/secret/key.php'; 
+
+// Entry point so l'App engine can detect the webpage path
 switch (@parse_url($_SERVER['REQUEST_URI'])['path']) {
     case '/':
-        require_once __DIR__.'/www/entry_form.php';
+        require_once __DIR__.'/www/entry_form.php'; // If nothing is in the path, it displays the main form
         break;
     case '/makeroute':
-        break;
-    case '/test':
-        require_once __DIR__.'/www/test.php';
         break;
     default:
         http_response_code(404);
